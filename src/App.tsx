6453f310d34589fad9f262aa5a2e723542177391
Jaiboon <kantapon.chaytip@outlook.com>
2023-12-10 22:54:18 +0700

    edit App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,37 +5,76 @@ import "./App.css";
 function App() {
   const [message, setMessage] = useState("");
   const [error, setError] = useState("");
+  const [pictureUrl, setPictureUrl] = useState("");
+  const [displayName, setDisplayName] = useState("");
+
+  const initializeLiff = async () => {
+    try {
+      await liff.init({
+        liffId: import.meta.env.VITE_LIFF_ID
+      });
+
+      //setMessage("LIFF init succeeded.");
+
+      // login
+      if (!liff.isLoggedIn()) {
+        try {
+          await liff.login();
+        } catch (error) {
+          console.error(error);
+        }
+      } else {
+        const accessToken = liff.getIDToken();
+        console.log(accessToken);
+      }
+
+      // Fetch user profile
+      fetchUserProfile();
+    } catch (e) {
+      setMessage("LIFF init failed.");
+      setError(`${e}`);
+    }
+  };
+
+  const fetchUserProfile = async () => {
+    try {
+      const profile = await liff.getProfile();
+      const userProfile = profile.userId;
+      const userDisplayName = profile.displayName;
+      const statusMessage = profile.statusMessage;
+      const userPictureUrl = profile.pictureUrl;
+
+      
+      setDisplayName(userDisplayName);
+      setPictureUrl(userPictureUrl);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   useEffect(() => {
-    liff
-      .init({
-        liffId: import.meta.env.VITE_LIFF_ID
-      })
-      .then(() => {
-        setMessage("LIFF init succeeded.");
-      })
-      .catch((e: Error) => {
-        setMessage("LIFF init failed.");
-        setError(`${e}`);
-      });
-  });
+    initializeLiff();
+  }, []);
 
   return (
     <div className="App">
-      <h1>create-liff-app</h1>
+      <h1>Profile</h1>
       {message && <p>{message}</p>}
       {error && (
         <p>
           <code>{error}</code>
         </p>
       )}
-      <a
-        href="https://developers.line.biz/ja/docs/liff/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        LIFF Documentation
-      </a>
+      <h2>{displayName}</h2>
+      <div>
+        <img src={pictureUrl} alt="profile" style={{ width: '350px', height: '350px' }} />
+      </div>
+      <div style={{ marginTop: '20px', }}>
+        <button onClick={() => {
+          liff.logout();
+          window.location.reload();
+        }}>Log out</button>
+      </div>
     </div>
   );
 }
