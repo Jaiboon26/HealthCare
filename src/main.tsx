60de6816906eb7889c97e1ef8c80ccfb42bb7346
Jaiboon <kantapon.chaytip@outlook.com>
2023-12-12 23:59:27 +0700

    config file for host

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+
+let root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 import UserPage from "./UserPage";
 import AddMedicPage from "./AddMedicPage";
@@ -39,9 +41,8 @@ const router = createBrowserRouter([
   }
 ])
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
-  document.getElementById("root")
 );
