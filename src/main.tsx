2a27795b1357c8b9366077413bcad37711cccc38
Jaiboon <kantapon.chaytip@outlook.com>
2023-12-13 01:10:43 +0700

    roll back to BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import {
   createBrowserRouter,
   RouterProvider,
-  createHashRouter,
 } from "react-router-dom";
 
 let root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
@@ -15,18 +14,32 @@ import MemberPage from "./MemberPage";
 import NotiManagePage from "./NotiManagePage";
 import HomePage from "./HomePage";
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
-    path: "/",
-    element: <HomePage />,
-    children: [
-      {
-        path: "UserPage",
-        element: <UserPage />,
-      },
-    ],
+    path: "/HealthCare",
+    element: <HomePage />
   },
-]);
+  {
+    path: "/UserPage",
+    element: <UserPage />
+  },
+  {
+    path: "/AddMedicPage",
+    element: <AddMedicPage />
+  },
+  {
+    path: "/MedicDetailPage",
+    element: <MedicDetailPage />
+  },
+  {
+    path: "/MemberPage",
+    element: <MemberPage />
+  },
+  {
+    path: "/NotiManagePage",
+    element: <NotiManagePage />
+  }
+])
 
 root.render(
   <React.StrictMode>
