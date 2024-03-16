5de4536699189778755ac6d678f5dcea3099dbff
Jaiboon <kantapon.chaytip@outlook.com>
2024-03-16 19:05:47 +0700

    Build for change url picture

--- src/AddMedicPage.tsx
+++ src/AddMedicPage.tsx
@@ -181,7 +181,7 @@ function AddMedicPage() {
         });
 
         const data = responseFind.data;
-        // console.log(data);
+        console.log(data);
         setMedicName("");
         setAfbf("Before");
         setMorning(false);
