ad6afc314bb6a127e83b6cc791c3e129def976c3
Jaiboon <kantapon.chaytip@outlook.com>
2024-03-24 00:59:11 +0700

    Fix can update if image null

--- src/MedicDetailPage.tsx
+++ src/MedicDetailPage.tsx
@@ -237,13 +237,6 @@ function MedicDetailPage() {
                   </svg>
                 </SvgIcon>
               </IconButton>
-              <IconButton aria-label="delete" sx={{ position: 'absolute', top: '30%', right: '0' }} onClick={() => DeleteMedic(medic.MedicName)}>
-                <SvgIcon sx={{ bgcolor: '#3B5998', color: 'white', padding: '5px', borderRadius: '100%' }}>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-                  </svg>
-                </SvgIcon>
-              </IconButton>
               <CardContent sx={{ flex: '1 0 auto', marginRight: '10px', display: 'flex', flexDirection: 'column', gap: '5px' }}>
                 <Typography component="div" variant="h5">
                   {medic.MedicName}
