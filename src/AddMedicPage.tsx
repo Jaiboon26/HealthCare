4c1041fe50df840c14ed8a27282515a5430b1f74
Jaiboon <kantapon.chaytip@outlook.com>
2024-04-18 21:23:13 +0700

    fix bug "if picture null can't add Medic" -> "Can add medic if picture null"

--- src/AddMedicPage.tsx
+++ src/AddMedicPage.tsx
@@ -80,17 +80,22 @@ function AddMedicPage() {
   const [userID, setUserID] = useState("Uc1e97d3b9701a31fba1f9911852eeb8f");
 
   const uploadFile = async () => {
-    if (imageUpload == null) return;
-    const imageRef = ref(storage, `${userIDChoose}/${imageUpload.name + v4()}`);
-    try {
-      const snapshot = await uploadBytes(imageRef, imageUpload);
-      const url = await getDownloadURL(snapshot.ref);
-      setImageUrls((prev) => [...prev, url]);
-      console.log("Success");
-      console.log(url);
-      setUrlImage(url);
-    } catch (error) {
-      console.error("Error uploading image:", error);
+    if (imageUpload == null) {
+      updateMedic();
+      insertMedic();
+    }
+    else {
+      const imageRef = ref(storage, `${userIDChoose}/${imageUpload.name + v4()}`);
+      try {
+        const snapshot = await uploadBytes(imageRef, imageUpload);
+        const url = await getDownloadURL(snapshot.ref);
+        setImageUrls((prev) => [...prev, url]);
+        console.log("Success");
+        console.log(url);
+        setUrlImage(url);
+      } catch (error) {
+        console.error("Error uploading image:", error);
+      }
     }
   };
 
